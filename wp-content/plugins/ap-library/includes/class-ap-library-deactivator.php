fa7e79ddbf464acbca01d380bf72a1d5f45439c5
antoninp <antoninpuleo@gmail.com>
2025-06-21 14:10:56 +1200

    fix: unregister cpt and taxo on desactivation

--- wp-content/plugins/ap-library/includes/class-ap-library-deactivator.php
+++ wp-content/plugins/ap-library/includes/class-ap-library-deactivator.php
@@ -30,7 +30,18 @@ class Ap_Library_Deactivator {
 	 * @since    1.0.0
 	 */
 	public static function deactivate() {
+		
+		// Unregister custom post types
+		unregister_post_type( 'aplb_uploads' );
+		unregister_post_type( 'aplb_library' );
 
+		// Unregister taxonomies if needed
+		unregister_taxonomy( 'aplb_uploads_tdate' );
+		unregister_taxonomy( 'aplb_uploads_genre' );
+		unregister_taxonomy( 'aplb_library_category' );
+
+		// Flush rewrite rules to remove CPT/taxonomy permalinks
+		flush_rewrite_rules();
 	}
 
 }
