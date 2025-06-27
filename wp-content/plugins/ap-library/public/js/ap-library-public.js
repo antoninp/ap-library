4476253ea496a7aff5983d8dfa8735964df7076e
antoninp <antoninpuleo@gmail.com>
2025-06-27 21:58:07 +1200

    upd: trying to fit single image gallery

--- wp-content/plugins/ap-library/public/js/ap-library-public.js
+++ wp-content/plugins/ap-library/public/js/ap-library-public.js
@@ -29,20 +29,52 @@
      * practising this, we should strive to set a better example in our own work.
      */
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const toTop = document.querySelector('.back-to-top');
-        if (!toTop) return;
-        window.addEventListener("scroll", function() {
-            if(window.pageYOffset > 100) {
-                toTop.classList.add('active-to-top');
-            } else {
-                toTop.classList.remove('active-to-top');
+    $(function(){
+
+        // Back to top button logic
+        var $toTop = $('.back-to-top');
+        if ($toTop.length) {
+            $(window).on('scroll', function() {
+                if(window.pageYOffset > 100) {
+                    $toTop.addClass('active-to-top');
+                } else {
+                    $toTop.removeClass('active-to-top');
+                }
+            });
+            $toTop.on('click', function(e) {
+                e.preventDefault();
+                window.scrollTo({top: 0, behavior: 'smooth'});
+            });
+        }
+
+        // Meow Gallery single image height fix
+        $('.wp-block-meow-gallery-gallery .mg-images').each(function(){
+            var $container = $(this);
+            var $images = $container.find('img');
+            if ($images.length === 1) {
+                $container.css('height', 'auto');
+                $images.css({
+                    'max-height': '400px',
+                    'height': 'auto',
+                    'width': 'auto',
+                    'object-fit': 'contain',
+                    'margin-left': 'auto',
+                    'margin-right': 'auto',
+                    'display': 'block'
+                });
             }
         });
-        toTop.addEventListener('click', function(e) {
-            e.preventDefault();
-            window.scrollTo({top: 0, behavior: 'smooth'});
+        $('.wp-block-meow-gallery-gallery .mg-images').each(function(){
+            var $container = $(this);
+            var $rows = $container.find('.mgl-row');
+            if ($rows.length === 1) {
+                $rows.css({
+                    'height': 'auto',
+                    'max-height': '400px'
+                });
+            }
         });
+
     });
 
 })( jQuery );
